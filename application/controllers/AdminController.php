2a0591f730be19c1b2bd75e2d31cec66372b36af
AleZek <zechini.alessandro@gmail.com>
2016-11-25 01:31:53 +0100

    admin uliveti

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -2,10 +2,13 @@
 
 class AdminController extends Zend_Controller_Action
 {
-
+    private $_ulivetoform;
+    private $_front;
     public function init()
     {
-        /* Initialize action controller here */
+        $this->_helper->layout->setLayout('adminlayout');
+
+        $this->_ulivetoform = new Application_Form_Aggiungiuliveto();
     }
 
     public function indexAction()
@@ -27,7 +30,99 @@ class AdminController extends Zend_Controller_Action
 
     }
 
+    public function elencoulivetiAction(){
+        $model = new Application_Model_UlivetoModel();
+        $uliveti = $model->getUliveti();
+        $aggiungiulivetoform = new Application_Form_Aggiungiuliveto();
+        $this->view->assign('listauliveti', $uliveti);
+        
+    }
 
+    public function elencoappezzamentiAction(){
+        $idUliveto = $this->getParam('id');
+        $modelAppezz = new Application_Model_AppezzamentoModel();
+        if(!is_null($idUliveto)){
+            $appezzamenti = $modelAppezz->getAppezzamenti();
+            $this->view->assign('appezzamenti', $appezzamenti);
+        }
+    }
+
+    public function aggiungiulivetoAction(){
+        $this->_ulivetoform->setAction(Zend_Controller_Front::getInstance()->getBaseUrl().'/admin/validateuliveto');
+        $this->view->assign('aggiungiform', $this->_ulivetoform);
+    }
+    public function validateulivetoAction(){
+        $request = $this->getRequest();
+        //istanzio la form di registrazione di un nuovo utente
+
+        if (!$request->isPost()) {
+            return $this->_helper->redirector('aggiungiuliveto');
+        }
+
+        $form = $this->_ulivetoform;
+
+        if (!$form->isValid($request->getPost())) {
+            $form->setDescription('Attenzione: alcuni dati inseriti sono errati.');
+            return $this->render('aggiungiuliveto');
+        }
+        else
+        {
+            $datiform=$this->_ulivetoform->getValues();
+
+            $ulivetomodel=new Application_Model_UlivetoModel();
+
+                $ulivetomodel->inserisciuliveto($datiform);
+                $this->getHelper('Redirector')->gotoSimple('elencouliveti','admin', $module = null);
+        }
+
+    }
+
+    public function eliminaulivetoAction(){
+        $id=$this->getParam('id');
+        if (!is_null($id)){
+            $ulivetomodel = new Application_Model_UlivetoModel();
+            $ulivetomodel->eliminauliveto($id);
+        }
+        $this->getHelper('Redirector')->gotoSimple('elencouliveti','admin', $module = null);
+    }
+
+    public function modificaulivetoAction(){
+        $request = $this->getRequest();
+        if ($request->isGet()){
+            $id = $this->getParam('id');
+            $ulivetomodel = new Application_Model_UlivetoModel();
+            $sql = $ulivetomodel->getulivetobyid($id)->current();
+            $data = array('descrizione' => $sql->descrizione);
+            $this->_ulivetoform->populate($data);
+        }
+        $this->_ulivetoform->setAction(Zend_Controller_Front::getInstance()->getBaseUrl().'/admin/validateulivetomod/id/'.$id);
+        $this->view->assign('aggiungiform', $this->_ulivetoform);
+    }
+
+    public function validateulivetomodAction(){
+        $request = $this->getRequest();
+        $id = $this->getParam('id');
+        if (!$request->isPost()) {
+            return $this->_helper->redirector('modificauliveto');
+        }
+
+        $form = $this->_ulivetoform;
+
+        if (!$form->isValid($request->getPost())) {
+            $form->setDescription('Attenzione: alcuni dati inseriti sono errati.');
+            return $this->render('modificauliveto');
+        }
+        else
+        {
+            $datiform=$this->_ulivetoform->getValues();
+            
+            $ulivetomodel=new Application_Model_UlivetoModel();
+           
+            $ulivetomodel->modificauliveto($datiform, $id);
+            $this->getHelper('Redirector')->gotoSimple('elencouliveti','admin', $module = null);
+        }
+
+    }
 }
 
 
